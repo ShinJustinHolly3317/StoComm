7501408b40288d81c27b970067e6219c8af32ad3
ShinJustinHolly3317 <83599048+ShinJustinHolly3317@users.noreply.github.com>
2021-10-26 09:51:23 +0800

    add: front end canvas can resize

--- public/javascript/canvas.js
+++ public/javascript/canvas.js
@@ -5,6 +5,11 @@ let toolType = 'brush'
 let drawId = 0
 let recievedDrawId = 0
 
+const canvasEle = {
+  canvas: document.querySelector('#canvas'),
+  ctx: canvas.getContext('2d')
+}
+
 // function
 function sendDrawHistory() {
   socket.emit('send_draw_history', drawHistory)
@@ -42,10 +47,11 @@ function resumeHistory(ctx, drawHistoryItem) {
   ctx.moveTo(x, y)
 }
 
+
 // lisener
 window.addEventListener('load', () => {
-  const canvas = document.querySelector('#canvas')
-  const ctx = canvas.getContext('2d')
+  // const canvas = document.querySelector('#canvas')
+  // const ctx = canvas.getContext('2d')
 
   // loading drawing history
   socket.on('take_draw_history', (drawHistoryFromServer) => {
@@ -56,8 +62,8 @@ window.addEventListener('load', () => {
   })
 
   // resizing the canvas
-  canvas.height = window.innerHeight * 0.9
-  canvas.width = window.innerWidth
+  canvasEle.canvas.height = window.innerHeight * 0.9
+  canvasEle.canvas.width = window.innerWidth
 
   // draw rect
   // ctx.fillStyle = 'rgb(121, 209, 201)'
@@ -80,7 +86,7 @@ window.addEventListener('load', () => {
 
   // default image
   const defaultImg = document.querySelector('#default-img')
-  ctx.drawImage(
+  canvasEle.ctx.drawImage(
     defaultImg,
     window.innerWidth / 2 - defaultImg.width / 2,
     window.innerHeight / 2 - defaultImg.height / 2
@@ -94,7 +100,7 @@ window.addEventListener('load', () => {
 
   function startPosition(e) {
     isPainting = true
-    ctx.beginPath()
+    canvasEle.ctx.beginPath()
     draw(e)
   }
 
@@ -107,55 +113,52 @@ window.addEventListener('load', () => {
     if (!isPainting) return
     console.log(toolType)
     if (toolType === 'brush') {
-      ctx.globalCompositeOperation = 'source-over'
-      ctx.strokeStyle = 'rgb(240,80,80)'
-      ctx.lineWidth = 10
+      canvasEle.ctx.globalCompositeOperation = 'source-over'
+      canvasEle.ctx.strokeStyle = 'rgb(240,80,80)'
+      canvasEle.ctx.lineWidth = 10
     } else if (toolType === 'eraser') {
-      ctx.globalCompositeOperation = 'destination-out'
-      ctx.strokeStyle = 'rgba(0,0,0,1)'
-      ctx.lineWidth = 30
+      canvasEle.ctx.globalCompositeOperation = 'destination-out'
+      canvasEle.ctx.strokeStyle = 'rgba(0,0,0,1)'
+      canvasEle.ctx.lineWidth = 30
     }
-    ctx.lineCap = 'round'
+    canvasEle.ctx.lineCap = 'round'
 
     // storing prevx, prevy
     prevX = e.clientX
     prevY = e.clientY
 
-    ctx.lineTo(e.clientX, e.clientY)
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.moveTo(e.clientX, e.clientY)
+    canvasEle.ctx.lineTo(e.clientX, e.clientY)
+    canvasEle.ctx.stroke()
+    canvasEle.ctx.beginPath()
+    canvasEle.ctx.moveTo(e.clientX, e.clientY)
 
     drawHistory.push({ location: [e.clientX, e.clientY], toolType, drawId })
     sendDrawHistory()
   }
 
-  // function offsetDraw() {
-  //   ctx.beginPath()
-  //   if (drawHistory[drawHistory.length-1]) {
-  //     console.log(
-  //       drawHistory[drawHistory.length-1][0],
-  //       drawHistory[drawHistory.length-1][1]+500
-  //     )
-  //     ctx.beginPath()
-  //     ctx.lineTo(
-  //       drawHistory[drawHistory.length-1][0],
-  //       drawHistory[drawHistory.length-1][1]+500
-  //     )
-  //     ctx.stroke()
-  //   }
-  // }
-
   // listener
-  canvas.addEventListener('mousedown', startPosition)
-  canvas.addEventListener('mouseup', finishPosition)
-  canvas.addEventListener('mousemove', draw)
+  canvasEle.canvas.addEventListener('mousedown', startPosition)
+  canvasEle.canvas.addEventListener('mouseup', finishPosition)
+  canvasEle.canvas.addEventListener('mousemove', draw)
 })
 
-// window.addEventListener('resize', () => {
-//   canvas.height = window.innerHeight
-//   canvas.width = window.innerWidth
-// })
+window.addEventListener('resize', () => {
+  canvasEle.canvas.height = window.innerHeight
+  canvasEle.canvas.width = window.innerWidth
+  // resume default img
+  const defaultImg = document.querySelector('#default-img')
+  canvasEle.ctx.drawImage(
+    defaultImg,
+    window.innerWidth / 2 - defaultImg.width / 2,
+    window.innerHeight / 2 - defaultImg.height / 2
+  )
+
+  // resume history
+  canvasEle.ctx.beginPath()
+  for (let item of drawHistory) {
+    resumeHistory(canvasEle.ctx, item)
+  }
+})
 
 toolSelection.addEventListener('change', () => {
   toolType = toolSelection.value
